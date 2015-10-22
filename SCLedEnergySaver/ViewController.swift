5f4d5034c76ad802fc7d960d4367ca779a2fbf67
Joel Costa <joelrfcosta@gmail.com>
2015-10-22 19:31:28 +0100

    Initial Commit

---
+++ SCLedEnergySaver/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SCLedEnergySaver
+//
+//  Created by Joel Costa on 22/10/15.
+//  Copyright © 2015 SadCoat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
